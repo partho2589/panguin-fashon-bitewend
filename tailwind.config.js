2f10f8b7f2f2bef8f4cc84fe82f9b0155349062b
partho2589 <parthomalo615@.com>
2022-02-20 15:46:52 +0600

    added fiels

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//     content: ["./src/**/*.{html,js}"],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
+
+function Salare(karos, mojut){
+  var dipias = karos - mojut;
+  return dipias;
+}
+var curanto = Salare(90000, 50000);
+console.log( 'a mar kace ace ai taka',curanto);
